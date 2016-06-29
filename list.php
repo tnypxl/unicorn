02c6931d00be6639e5a8e4156ed7acd085165725
xpxautomationlab <xpanxionjenkins@gmail.com>
2016-06-29 13:26:07 -0500

    Update list.php

--- list.php
+++ list.php
@@ -14,7 +14,6 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         #echo "Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
-        print_r($row);
         echo "Name: ".$row['firstName']." ".$row["lastName"]." Phone: Email: Notes: <br />";
     }
 } else {
