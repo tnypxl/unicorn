1f9689e642799fffdf226fab741932a9da6ea4b3
xpxautomationlab <xpanxionjenkins@gmail.com>
2016-06-29 13:13:06 -0500

    Update list.php

--- list.php
+++ list.php
@@ -1,6 +1,25 @@
 <?php
 
 
-echo "This is a list!";
+require 'connection.php';
+$conn = Connect();
+$query = "select * from people order by lastName asc;";
+$result = $conn->query($query); 
+if (!$success) {
+    die("Couldn't retrieve data: ".$conn->error);
+}
+ 
+#echo "This is my list<br>";
 
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        #echo "Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
+        print_r($row);
+    }
+} else {
+    echo "0 results";
+}
+ 
+$conn->close();
 ?>
